83d8a2f9fae67ff2e9ccbec9749c578d4db3cec3
jaythaceo <jaythaceo@gmail.com>
2014-12-29 17:05:48 -0500

    [chore] fixed destroy method

--- pin_board/app/controllers/pins_controller.rb
+++ pin_board/app/controllers/pins_controller.rb
@@ -2,6 +2,7 @@ class PinsController < ApplicationController
   before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def show
@@ -11,21 +12,37 @@ class PinsController < ApplicationController
     @pin = Pin.new
   end
 
-    def create
-      @pin = Pin.new(pin_params)
+  def create
+    @pin = Pin.new(pin_params)
 
-      if @pin.save
-        redirect_to @pin, notice: "Successfully created new pin"
-      else
-        render 'new'
-      end
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new pin"
+    else
+      render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was Successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
 
 
-    private
+  private
 
   def pin_params
-	params.require(:pin).permit(:title, :description)
+    params.require(:pin).permit(:title, :description)
   end
 
   def find_pin
