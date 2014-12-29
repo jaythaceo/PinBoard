fa2871d43e361fe9420b0c7f6fb894ff2952ac9f
jaythaceo <jaythaceo@gmail.com>
2014-12-29 14:16:30 -0500

    [chore] fixed create method

--- pin_board/app/controllers/pins_controller.rb
+++ pin_board/app/controllers/pins_controller.rb
@@ -1,32 +1,34 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
-	def index
-	end
+  def index
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@pin = Pin.new
-	end
+  def new
+    @pin = Pin.new
+  end
 
-	def create
-		@pin = Pin.new(pin_params)
+    def create
+      @pin = Pin.new(pin_params)
 
-		if @pin.save
-			redirect_to @pin, notice: "successfully created new Pin"
-		else
-			render 'new'
-        end
+      if @pin.save
+        redirect_to @pin, notice: "Successfully created new pin"
+      else
+        render 'new'
+      end
     end
-	private
 
-	def pin_params
-		params.require(:pin).permit(:title, :description)
-	end
 
-	def find_pin
-		@pin = Pin.find(params[:id])
-	end
+    private
+
+  def pin_params
+	params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+   @pin = Pin.find(params[:id])
+  end
 end
